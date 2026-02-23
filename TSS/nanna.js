ec47a5c5cc0d6de9ecb52045ee372a8825fa91dd
nanna <navkishoredas@gmail.com>
2026-02-23 10:24:37 +0530

    all projects are there

---
+++ TSS/nanna.js
@@ -0,0 +1,5 @@
+function nannaString() {
+    var nanna = 16;
+    return nanna.toString();
+}
+console.log(nannaString()); // Output: "16"
---
+++ TSS/nanna.ts
@@ -0,0 +1,10 @@
+function nannaString() {
+  var nanna = 16;
+  function gettingNanna() {
+    var nanna = "nanna Dashing";
+    console.log(nanna);
+  }
+  gettingNanna();
+  return nanna.toString();
+}
+console.log(nannaString());
